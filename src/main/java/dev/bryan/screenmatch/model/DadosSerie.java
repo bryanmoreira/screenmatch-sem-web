f195600f68718eaed4621b2185d55ddc5aff5345
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2024-01-29 21:01:56 -0300

    First commit

---
+++ src/main/java/dev/bryan/screenmatch/model/DadosSerie.java
@@ -0,0 +1,10 @@
+package dev.bryan.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
---
+++ src/main/java/dev/bryan/screenmatch/service/IConverteDados.java
@@ -0,0 +1,5 @@
+package dev.bryan.screenmatch.service;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+}

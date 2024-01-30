13eeeb15635c4ac2fa23fa94313565493593f05f
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2024-01-30 20:21:25 -0300

    Adding classes DadosEpisodio, DadosTemporada, Episodio and Principal

--- src/main/java/dev/bryan/screenmatch/ScreenmatchApplication.java
+++ src/main/java/dev/bryan/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package dev.bryan.screenmatch;
 
-import dev.bryan.screenmatch.model.DadosSerie;
-import dev.bryan.screenmatch.service.ConsumoApi;
-import dev.bryan.screenmatch.service.ConverteDados;
+import dev.bryan.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=supernatural&apikey=f461ddd0");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
